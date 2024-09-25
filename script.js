0c2863b3c8514e09860be82eaaad7e454fe1ba7e
jomitaa <jsilc2200@alumno.ipn.mx>
2024-09-24 18:04:53 -0600

    progra web

---
+++ script.js
@@ -0,0 +1,33 @@
+// || Clase: 24/09/24
+/* || Nombre: Sil cruz Jose Maria
+              Martinez Quiroz Leonardo
+*/
+
+
+
+//varaibles
+/*
+las variables guardan infromacion qu puedes reutilizar
+*/
+
+//Declaracion de variables
+
+let nombre = 'TfChema' // Las variables let son las que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 15;
+
+// Operadores
+
+let a = 5;
+let b = 2;
+let suma = a + b;
+let resta = a - b;
+let multiplicacoin = a * b;
+let divicion = a / b;
+
+//operadore sde comparacion
+
+a > b; //true
+a < b;//false
+a == b; //false
+a === b;//false (compara)
