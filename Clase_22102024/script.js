36281eb9abdb7c9b199d32be49214cdc5f9da1bf
SIL CRUZ JOSE MARIA <160265839+jomitaa@users.noreply.github.com>
2024-10-22 17:43:23 -0600

    clase 2210204

---
+++ Clase_22102024/script.js
@@ -0,0 +1,21 @@
+function hola(a,b){
+    return a+b;
+}
+console.log(hola(3,5));
+
+// CONVERTIR A FUNCION FLECHA
+
+const suma = (a,b) => {
+    return a + b;
+};
+
+console.log(suma(3,10));
+
+// SIMPLIFICAR
+
+const suma2 = (a,b) => a+b;
+console.log(suma2(2,1));
+
+// 04 solo un parametro
+const cuadrado = x => x*x;
+console.log(cuadrado(3));
