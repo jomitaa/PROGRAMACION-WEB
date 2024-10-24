a629437b0c965c8c172c2430b90f7ea648bc8e1e
SIL CRUZ JOSE MARIA <160265839+jomitaa@users.noreply.github.com>
2024-10-24 16:36:33 -0600

    clase 24102024

---
+++ Clase_24102024/script.js
@@ -0,0 +1,39 @@
+//Arreglos
+/*let array = ["dato1", 1, Boolean];
+console.log("tamaño del array:", + array.length)
+
+array.push("inserto nuevo valor")
+console.log(array[3])
+
+console.log("nuevo tamaño del array:",array.length)
+
+let array = [];
+let i = 0;
+
+
+for ( i=0; i<=10; i++){
+    array.push(i*5);
+
+    console.log("5 * ",i , " = ",array[i])
+
+    if(array.length >=7){
+        console.log("fin arreglo")
+        break;
+    }
+
+}
+
+
+
+let array = ["dato1", "dato2", "dato3"];
+array.push("dato4")
+
+//Eliminar
+
+array.splice(1,1);
+console.log(array);
+*/
+const tarea = document.getElementById("input") 
+if(tarea === null){
+    alert("Es necesario agregar una tarea")
+}
